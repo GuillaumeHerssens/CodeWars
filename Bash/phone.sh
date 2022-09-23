9a34a001a50282c4ba7f182f3e0edc1cccd1d2f4
GuillaumeHerssens <89980132+GuillaumeHerssens@users.noreply.github.com>
2022-09-23 17:00:48 +0200

    Added description

--- Bash/phone.sh
+++ Bash/phone.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Write a function that accepts a string, and returns true if it is in the form of a phone number.
+# Assume that any integer from 0-9 in any of the spots will produce a valid phone number.
+
+# Only worry about the following format:
+# (123) 456-7890 (don't forget the space after the close parentheses)
+
+# Examples:
+# "(123) 456-7890"  => true
+# "(1111)555 2345"  => false
+# "(098) 123 4567"  => false
+
 if [[ $1 =~ (^"("[0-9]{3}") "[0-9]{3}"-"[0-9]{4}$) ]];then echo "True"
 else echo "False"
 fi
